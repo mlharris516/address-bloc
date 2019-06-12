246beae8c1858af7a6cc72ae0cdafdeb3f21f16a
Meredith Harris <mlharris516@gmail.com>
2019-06-11 19:24:17 -0600

    first attempt prompt and menu assignment

---
+++ controllers/MenuController.js
@@ -0,0 +1,63 @@
+const inquirer = require('inquirer');
+
+module.exports = class MenuController {
+    constructor(){
+        this.mainMenuQuestions = [
+            {
+                type: "list",
+                name: "mainMenuChoice",
+                message: "Please choose from an option below: ",
+                choices: [
+                    "Add new contact",
+                    "Get Date",
+                    "Exit"
+                ]
+            }
+        ];
+        this.contacts = [];
+    }
+
+    main() {
+        console.log(`Welcome to AddressBloc!`);
+        inquirer.prompt(this.mainMenuQuestions).then((response) => {
+            switch(response.mainMenuChoice){
+                case "Add new contact":
+                    this.addContact();
+                    break;
+                case "Get Date":
+                    this.getDate();
+                    break;
+                case "Exit":
+                    this.exit();
+                    break;
+                default:
+                    console.log("Invalid input");
+                    this.main();
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    clear() {
+        console.log("\x1Bc");
+    }
+
+    addContact() {
+        this.clear();
+        console.log('addContact called');
+        this.main();
+    }
+
+    exit() {
+        console.log("Thanks for using AddressBloc!");
+        process.exit();
+    }
+
+    getDate() {
+        const currentTS = new Date();
+        console.log(currentTS);
+        this.main();
+    }
+}
