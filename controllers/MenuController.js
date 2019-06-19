34a02971190d19999cb39200e5d28aec82918620
Meredith Harris <mlharris516@gmail.com>
2019-06-18 19:44:39 -0600

    finished checkpoint work

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,4 +1,5 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const ContactController = require("./ContactController");
 
 module.exports = class MenuController {
     constructor() {
@@ -13,13 +14,20 @@ module.exports = class MenuController {
                 ]
             }
         ];
-        this.contacts = [];
+        this.book = new ContactController();
     }
 
     addContact() {
         this.clear();
-        console.log('addContact called');
-        this.main();
+        inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+            this.book.addContact(answers.name, answers.phone).then((contact) => {
+                console.log("Contact added successfully!");
+                this.main();
+            }).catch((err) => {
+                console.log(err);
+                this.main();
+            });
+        });
     }
 
     clear() {
@@ -30,10 +38,6 @@ module.exports = class MenuController {
         console.log("Thanks for using AddressBloc!");
         process.exit();
     }
-
-    getContactCount(){
-        return this.contacts.length;
-      }
 
     main() {
         console.log(`Welcome to AddressBloc!`);
